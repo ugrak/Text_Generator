cae46eb930b5516138bc145ad95221398fe10dcb
ugrak <augrak@yandex.ru>
2021-10-13 14:46:53 +0300

    Second stage

--- text_generator.py
+++ text_generator.py
@@ -1,17 +1,20 @@
 from nltk.tokenize import WhitespaceTokenizer
+from nltk import bigrams
 
 filename = input()
 tk = WhitespaceTokenizer()
 with open('./' + filename, 'r', encoding="utf-8") as file:
     tokens = tk.tokenize(file.read())
-    print(f'Corpus statistics\nAll tokens: {len(tokens)}\nUnique tokens: {len(set(tokens))}')
+    bigrams = list(bigrams(tokens))
+    print(f'Number of bigrams: {len(bigrams)}')
     while True:
         user_number = input()
         if user_number.lower() == 'exit':
             break
         else:
             try:
-                print(tokens[int(user_number)])
+                bgrm = bigrams[int(user_number)]
+                print(f'Head: {bgrm[0]} Tail: {bgrm[0]}')
             except ValueError:
                 print('Type Error. Please input an integer.')
             except IndexError:
