6550b0e72c59df02376d391a8fe07ba3fe8e6680
ugrak <augrak@yandex.ru>
2021-10-17 15:46:19 +0300

    Third stage, created Markov chain.

--- text_generator.py
+++ text_generator.py
@@ -1,21 +1,26 @@
 from nltk.tokenize import WhitespaceTokenizer
 from nltk import bigrams
+from collections import Counter
 
 filename = input()
 tk = WhitespaceTokenizer()
 with open('./' + filename, 'r', encoding="utf-8") as file:
     tokens = tk.tokenize(file.read())
-    bigrams = list(bigrams(tokens))
-    print(f'Number of bigrams: {len(bigrams)}')
+    bigrams_list = list(bigrams(tokens))
+    bigrams_dict = {}
+    for element in bigrams_list:
+        bigrams_dict.setdefault(element[0], []).append(element[1])
+    for element in bigrams_list:
+        bigrams_dict[element[0]] = Counter(bigrams_dict[element[0]])
     while True:
-        user_number = input()
-        if user_number.lower() == 'exit':
+        user_word = input()
+        if user_word.lower() == 'exit':
             break
         else:
             try:
-                bgrm = bigrams[int(user_number)]
-                print(f'Head: {bgrm[0]} Tail: {bgrm[0]}')
-            except ValueError:
-                print('Type Error. Please input an integer.')
-            except IndexError:
-                print('Index Error. Please input an integer that is in the range of the corpus.')
+                content = bigrams_dict[user_word]
+                print(f'Head: {user_word}')
+                for key, value in content.most_common():
+                    print(f'Tail: {key}    Count: {value}')
+            except KeyError:
+                print('Key Error. The requested word is not in the model. Please input another word.')
