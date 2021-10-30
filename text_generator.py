233b194fc0c646a1761640d72d0a4c9a89feef37
ugrak <augrak@yandex.ru>
2021-10-30 13:21:30 +0300

    Fifth stage

--- text_generator.py
+++ text_generator.py
@@ -1,8 +1,29 @@
+import string
 from random import choice, choices
 from nltk.tokenize import WhitespaceTokenizer
 from nltk import bigrams
 from collections import Counter
-SENTENCE_LENGTH = 10
+SENTENCE_LENGTH = 5
+AMOUNT_OF_SENTENCES = 10
+
+
+def build_sentence(bgrms_dict):
+    for i in range(AMOUNT_OF_SENTENCES):
+        while True:
+            last_word = choice(list(bgrms_dict.keys()))
+            if last_word[0] in string.ascii_uppercase and last_word[-1] not in '.!?':
+                break
+        sentence = last_word
+        for j in range(SENTENCE_LENGTH - 2):
+            last_word = choices(list(bgrms_dict[last_word].keys()), weights=list(bgrms_dict[last_word].values()))[0]
+            sentence = sentence + ' ' + last_word
+        while True:
+            last_word = choices(list(bgrms_dict[last_word].keys()), weights=list(bgrms_dict[last_word].values()))[0]
+            if last_word[-1] in '.!?':
+                break
+        sentence = sentence + ' ' + last_word
+        print(sentence)
+
 
 filename = input()
 tk = WhitespaceTokenizer()
@@ -14,10 +35,4 @@ with open('./' + filename, 'r', encoding="utf-8") as file:
         bigrams_dict.setdefault(element[0], []).append(element[1])
     for element in bigrams_list:
         bigrams_dict[element[0]] = Counter(bigrams_dict[element[0]])
-    for i in range(SENTENCE_LENGTH):
-        last_word = choice(list(bigrams_dict.keys()))
-        sentence = last_word
-        for j in range(SENTENCE_LENGTH - 1):
-            last_word = choices(list(bigrams_dict[last_word].keys()), weights=list(bigrams_dict[last_word].values()))[0]
-            sentence = sentence + ' ' + last_word
-        print(sentence)
+    build_sentence(bigrams_dict)
